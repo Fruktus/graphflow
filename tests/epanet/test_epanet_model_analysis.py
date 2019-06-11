b8bcf218bea4d559762d36e6d91f41c89c9c41f6
Fruktus <fruktusek@gmail.com>
2019-06-11 19:01:59 +0200

    fixes

--- tests/epanet/test_epanet_model_analysis.py
+++ tests/epanet/test_epanet_model_analysis.py
@@ -1,92 +1,90 @@
-from graphflow.epanet.epanet_model_analysis import *
-from pathlib import Path
+# pylint: disable=R0801,W0401,W0614
 import pytest
+from pathlib import Path
+
+from graphflow.epanet.epanet_model_analysis import *
 from graphflow.epanet.epanet_model import EpanetFlowNetwork, SimulationType
 
 
 @pytest.fixture(scope='module')
 def test_network():
-    """
-    python3.7 -m graphflow epanet "/home/michal/Documents/io2019/graph-flow/examples/epanet/example_epanet_network.inp" earthquake --epicenter_x 25000 --epicenter_y 5000 --magnitude 8.0 --depth 1000
-    :return:
-    """
     base_path = Path(__file__).parent.parent.parent
     file_path = (base_path / "examples" / "epanet" / "example_epanet_network.inp").resolve()
-    test_network = EpanetFlowNetwork(file_path, SimulationType.PRESSURE, time=20)
-    return test_network
+    net = EpanetFlowNetwork(file_path, SimulationType.PRESSURE, time=20)
+    return net
 
 
-def test_degree_centrality(test_network):
-    assert isinstance(degree_centrality(test_network), dict)
+def test_degree_centrality(net):
+    assert isinstance(degree_centrality(net), dict)
 
 
-def test_hits(test_network):
-    assert isinstance(hits(test_network), tuple)
+def test_hits(net):
+    assert isinstance(hits(net), tuple)
 
 
-def test_diameter(test_network):
-    assert isinstance(diameter(test_network), int)
+def test_diameter(net):
+    assert isinstance(diameter(net), int)
 
 
-def test_density(test_network):
-    assert isinstance(density(test_network), float)
+def test_density(net):
+    assert isinstance(density(net), float)
 
 
-# def test_modularity(test_network):
-#     assert isinstance(modularity(test_network), None)
+# def test_modularity(net):
+#     assert isinstance(modularity(net), None)
 
 
-def test_page_rank(test_network):
-    assert isinstance(page_rank(test_network), dict)
+def test_page_rank(net):
+    assert isinstance(page_rank(net), dict)
 
 
-def test_eigenvector_centrality(test_network):
-    assert isinstance(eigenvector_centrality(test_network), dict)
+def test_eigenvector_centrality(net):
+    assert isinstance(eigenvector_centrality(net), dict)
 
 
-def test_closeness_centrality(test_network):
-    assert isinstance(closeness_centrality(test_network), dict)
+def test_closeness_centrality(net):
+    assert isinstance(closeness_centrality(net), dict)
 
 
-def test_betweenness_centrality(test_network):
-    assert isinstance(betweenness_centrality(test_network), dict)
+def test_betweenness_centrality(net):
+    assert isinstance(betweenness_centrality(net), dict)
 
 
-def test_average_path_centrality(test_network):
-    assert isinstance(average_path(test_network), float)
+def test_average_path_centrality(net):
+    assert isinstance(average_path(net), float)
 
 
-# def test_maximum_flow(test_network):  # TODO
-#     assert isinstance(maximum_flow(test_network, source, target),)
+# def test_maximum_flow(net):
+#     assert isinstance(maximum_flow(net, source, target),)
 
 
-def test_current_flow_closeness(test_network):
-    assert isinstance(current_flow_closeness(test_network), dict)
+def test_current_flow_closeness(net):
+    assert isinstance(current_flow_closeness(net), dict)
 
 
-def test_current_flow_betweenness(test_network):
-    assert isinstance(current_flow_betweenness(test_network), dict)
+def test_current_flow_betweenness(net):
+    assert isinstance(current_flow_betweenness(net), dict)
 
 
-def test_load_centrality(test_network):
-    assert isinstance(load_centrality(test_network), dict)
+def test_load_centrality(net):
+    assert isinstance(load_centrality(net), dict)
 
 
-def test_subgraph(test_network):
-    assert isinstance(subgraph(test_network), dict)
+def test_subgraph(net):
+    assert isinstance(subgraph(net), dict)
 
 
-def test_harmonic_centrality(test_network):
-    assert isinstance(harmonic_centrality(test_network), dict)
+def test_harmonic_centrality(net):
+    assert isinstance(harmonic_centrality(net), dict)
 
 
-def test_global_reaching(test_network):
-    assert isinstance(global_reaching(test_network), float)
+def test_global_reaching(net):
+    assert isinstance(global_reaching(net), float)
 
 
-def test_percolation(test_network):
-    assert isinstance(percolation(test_network), dict)
+def test_percolation(net):
+    assert isinstance(percolation(net), dict)
 
 
-def test_second_order_centrality(test_network):
-    assert isinstance(second_order_centrality(test_network), dict)
+def test_second_order_centrality(net):
+    assert isinstance(second_order_centrality(net), dict)
