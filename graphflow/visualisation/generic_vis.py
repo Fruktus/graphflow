9c209c57da73b2eb1886b043ee99d7d3449a59c5
pmaczuga <pmaczuga97@gmail.com>
2019-07-09 13:51:00 +0200

    Minor changes.

--- graphflow/visualisation/generic_vis.py
+++ graphflow/visualisation/generic_vis.py
@@ -1,4 +1,5 @@
 # pylint: disable=E1101
+import webbrowser
 
 import holoviews as hv
 from holoviews import opts
@@ -34,7 +35,8 @@ def visualize_holoviews(network, metrics: [tuple] = None):
     show(plot)
 
 
-def visualize_epidemic(network, simulation_investigation: EoN.Simulation_Investigation, metrics: [tuple] = None):
+def visualize_epidemic(network, simulation_investigation: EoN.Simulation_Investigation,
+                       metrics: [tuple] = None, max_time_steps=200):
     hv.extension('bokeh')
 
     nx_network = get_nx_network(network)
@@ -43,26 +45,34 @@ def visualize_epidemic(network, simulation_investigation: EoN.Simulation_Investi
         del node[1]['infected']
         del node[1]['recovered']
 
+    if simulation_investigation.SIR:
+        time_steps = simulation_investigation.t()[1:]
+    else:
+        time_steps = simulation_investigation.t()[1:max_time_steps]
+
     layout = nx.layout.spring_layout(nx_network)
-    graph_dict = {time: __get_network_graph(nx_network , layout, simulation_investigation.get_statuses(time=time), metrics) for time in simulation_investigation.t()[1:]}
+    graph_dict = {time: __get_epidemic_network_graph(nx_network, layout,
+                                                     simulation_investigation.get_statuses(time=time),
+                                                     metrics) for time in time_steps}
     hmap = hv.HoloMap(graph_dict, kdims='t')
 
     defaults = dict(width=700, height=700, padding=0.1)
     hv.opts.defaults(opts.EdgePaths(**defaults), opts.Graph(**defaults), opts.Nodes(**defaults))
 
-    renderer = hv.renderer('bokeh')
-    hv.save(hmap, "graph.html", backend='bokeh')
+    filename = "graph.html"
+    hv.save(hmap, filename, backend='bokeh')
+    webbrowser.open(filename)
 
 
-def __get_network_graph(network, layout, statuses, metrics: [tuple] = None):
+def __get_epidemic_network_graph(network, layout, statuses, metrics: [tuple] = None):
     if metrics:
         for i in metrics:
             if isinstance(i[1], dict):
                 nx.set_node_attributes(network, i[1], i[0])
             elif isinstance(i[1], tuple):
                 nx.set_node_attributes(network, i[1][0], i[0])
-            else:
-                print(i[0], ":", i[1])
+            # else:
+            #     print(i[0], ":", i[1])
 
     nx.set_node_attributes(network, statuses, 'Status')
     color_map = {'S': 'green', 'I': 'red', 'R': 'blue'}
